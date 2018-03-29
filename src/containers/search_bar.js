9c0bc96a012384de4e867a34afd7dc1d55d10bfc
golasowska <annagolasowska@gmail.com>
2018-03-29 14:12:54 +0200

    added weather_list component with charts - sparklines

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather} from "../actions";
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({
+        term: event.target.value
+    });
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.fetchWeather(this.state.term);
+    this.setState({
+        term: ''
+    })
+  }
+
   render() {
     return (
-      <form className="input-group">
-        <input />
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
       </form>
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 
 import App from './components/App';
 import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
